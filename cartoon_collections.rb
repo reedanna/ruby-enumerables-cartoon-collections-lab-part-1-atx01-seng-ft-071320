87fda47925379b39922de755a56e592ab17d4bc4
Anna Nicole Reed <reedanna@mail.com>
2020-06-26 00:59:36 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,6 +12,7 @@ def list_dwarves(array)
   #
   # Print a numbered list of each element
   array.each do |element, index|
-    p index + 1 + ". #{element}"
+    number = index + 1
+    p number + ". #{element}"
   end
 end
